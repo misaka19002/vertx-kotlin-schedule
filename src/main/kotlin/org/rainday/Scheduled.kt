650b7e79a8e4ef88a2644e7f553ea1dbe60a3a91
wyd <wyudi@qq.com>
2018-09-14 15:40:16 +0800

    vertx schedule

--- src/main/kotlin/org/rainday/Scheduled.kt
+++ src/main/kotlin/org/rainday/Scheduled.kt
@@ -2,6 +2,7 @@ package org.rainday
 
 import io.vertx.core.Vertx
 import org.quartz.CronExpression
+import java.time.LocalDateTime
 import java.util.*
 
 
@@ -9,21 +10,25 @@ import java.util.*
  * Created by wyd on 2018/9/12 14:36:59.
  */
 
-fun Vertx.scheduled(cron: String, block: (timerId: Long) -> Unit):  Unit{
+fun Vertx.scheduled(cron: String, ebusAddr: String):  Unit{
     //定时任务不考虑集群，只单机运行
     val expression = CronExpression(cron)
-    this.assignTask(expression, block)
+    this.assignTask(expression, ebusAddr)
 }
 
 
-private fun Vertx.assignTask(expression: CronExpression, block: (timerId: Long) -> Unit) {
-   this.timerStream(expression.getNextValidTimeAfter(Date()).toInstant().toEpochMilli() - Date().toInstant().toEpochMilli())
-            .handler(block)
-            .endHandler {
-                this.assignTask(expression, block)
+private fun Vertx.assignTask(expression: CronExpression, ebusAddr: String) {
+    this.setTimer(expression.getNextValidTimeAfter(Date()).toInstant().toEpochMilli() - Date().toInstant().toEpochMilli()){
+        println("${LocalDateTime.now()} send ($it) through eventbuss")
+        //send timerId
+        this.eventBus().send<String>(ebusAddr, it.toString()){
+            if (it.succeeded()) {
+                println("${LocalDateTime.now()} schedule receive response successfully! ${it.result().body()}")
+            } else {
+                println("schedule receive response failed :${it.cause().message}")
             }
-            .exceptionHandler {
-                println("vertx scheduled exception ${it.message}")
-            }
+            this.assignTask(expression, ebusAddr)
+        }
+    }
 }
 
--- src/test/kotlin/org/rainday/Test.kt
+++ src/test/kotlin/org/rainday/Test.kt
@@ -1,24 +1,25 @@
 package org.rainday
 
 import io.vertx.core.Vertx
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
+import io.vertx.kotlin.coroutines.dispatcher
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
 import java.util.*
 
 /**
  * Created by wyd on 2018/9/13 13:45:45.
  */
-
 fun main(vararg args: String) {
     val vertx = Vertx.vertx()
-    vertx.scheduled("0/2 * * * * ?") {
-        //请不要在这里执行阻塞任务
-        //请在这里自己处理异常
-        if (it > 0) {
-            throw RuntimeException("sdfdsf")
+    vertx.eventBus().consumer<String>("vertx.kotlin.schedule"){
+        launch(vertx.dispatcher()) {
+            println("consumer receive: ${it.body()}")
+            delay(10000)
+            it.reply(it.body())
         }
-        println("$it ${LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) } ${Thread.currentThread().id}  ${Thread.currentThread().name}")
+
     }
+    vertx.scheduled("0/2 * * * * ?","vertx.kotlin.schedule")
     Thread.sleep(6000)
 
     println("5      " + Date().toInstant().toEpochMilli() + "${Thread.currentThread().id}  ${Thread.currentThread().name}")
